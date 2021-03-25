f1927a7c86e904351918c8dfba7d1a98545bd70a
Boglárka Száday <boglarka.szaday@stud.uni-corvinus.hu>
2021-03-25 12:32:38 +0100

    javascript_hetedik_3/25_1

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,26 @@
+﻿window.onload = letoltes()
+var kérdések;
+
+function letoltes() {
+    fetch('/questions.json')
+        .then(response => response.json())
+        .then(data => letoltesBefejezodott(data));
+}
+
+function letoltesBefejezodott(d) {
+    console.log("Sikeres letoltes")
+    console.log(d)
+    kérdések = d;
+    kerdesMegjelenites(0);
+}
+
+function kerdesMegjelenites(kérdés) {
+    let ide_kérdés = document.getElementById("kérdés_szöveg");
+
+    for (var i = 0; i < kérdés; i++) {
+        console.log(kérdés[i].questionText)
+        let elem_kérdés = document.createElement("div")
+        elem_kérdés.innerHTML = kérdés[i].questionText
+        ide_kérdés.appendChild(elem_kérdés)
+    }
+}
